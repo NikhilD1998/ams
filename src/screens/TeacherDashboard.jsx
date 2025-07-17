a266f0f1fca7b8d4ffeb75618518e64a4553450c
NikhilD1998 <nikhildadab@hotmail.com>
2025-07-17 15:26:41 +0530

    feat: Add ParentDashboard, StudentDashboard, and TeacherDashboard components

---
+++ src/screens/TeacherDashboard.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const TeacherDashboard = () => {
+  return (
+    <View>
+      <Text>TeacherDashboard</Text>
+    </View>
+  );
+};
+
+export default TeacherDashboard;
